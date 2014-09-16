17da22c7a1dadf2d7c989856124cb29be04249a4
Aristobulo <aristobulo@example.com>
2014-09-16 11:05:20 -0430

    Agregando archivos para declarar modulo

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Curso OpenERP',
+    'description': 'Este módulo es para aprender OpenERP',
+    'author': 'Aristobulo',
+    'version': 'dia1',
+    'depends': ['base', ],
+    'data': [],
+    'demo': [],
+}
