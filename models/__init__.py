e49b57c3a42d2f35c5f2b6eb3f268f06fe7f0dde
Juan Marcano <jjmarcanoc@laptop-debian.casigua>
2014-09-16 17:24:53 -0430

    declaramos y agregamos todos los modulos necesarios para nuestro ejemplo

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 import suscriptor
+import suscripcion
+import categoria
+import tipo_medio
+import multimedia
+import tienda
+import stock
+import solicitud
--- models/suscriptor.py
+++ models/suscriptor.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+from openerp.osv import osv, fields
+
+
+class suscriptor(osv.osv):
+    _name = 'co.suscriptor'
+    _description = 'CO Suscriptor'
+
+    _columns = {
+        'name': fields.char('Nombre'),
+        'identification': fields.char('Cédula'),
+        'Adress': fields.text('Dirección'),
+        }
+
+suscriptor()
